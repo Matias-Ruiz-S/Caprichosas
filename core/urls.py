a22fbb711d36984dd190628d8877e4e3f9ebad82
SCerulean <zjeremy507@gmail.com>
2023-05-21 19:14:44 -0400

    fix url

--- core/urls.py
+++ core/urls.py
@@ -4,6 +4,12 @@ from .views import *
 
 
 urlpatterns = [
-
+    path("", home,name="home"),
+    path("listar/<slug>", listar,name="listar"),
+    path("template", template,name="template"),
+    path("nuevo-producto", Productoform,name="Productoform"),
+    path("crud", crud,name="crud"),
+    path("nueva-categoria", agreCategoria,name="agreCategoria"),
+    path("nuevo-ingrediente", agreIngrediente,name="agreIngrediente"),
 ]
 
