53c448b571bbe38eaf13a7061c3df153b0e3da73
ivanrisco1998 <131366701+ivanrisco1998@users.noreply.github.com>
2023-06-05 23:43:10 -0400

    Update settings.py

--- Caprichosas/settings.py
+++ Caprichosas/settings.py
@@ -73,8 +73,8 @@ TEMPLATES = [
 WSGI_APPLICATION = 'Caprichosas.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
+Database
+https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES={
     'default':
@@ -86,12 +86,12 @@ DATABASES={
     }
 }
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
